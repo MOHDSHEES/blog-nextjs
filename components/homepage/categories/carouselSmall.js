e2687c4a267d3e0aca99ccd1ee4f315579295a7c
MOHDSHEES <mohd.shees100@gmail.com>
2023-06-27 02:11:06 +0530

    static props

--- components/homepage/categories/carouselSmall.js
+++ components/homepage/categories/carouselSmall.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 var $ = require("jquery");
 if (typeof window !== "undefined") {
   window.$ = window.jQuery = require("jquery");
