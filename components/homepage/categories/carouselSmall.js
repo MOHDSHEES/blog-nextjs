0c97e231ff65d5c449f2733a74d9fa2e7864935b
MOHDSHEES <mohd.shees100@gmail.com>
2023-06-27 01:52:11 +0530

    static props

--- components/homepage/categories/carouselSmall.js
+++ components/homepage/categories/carouselSmall.js
@@ -9,62 +9,66 @@ import "owl.carousel/dist/assets/owl.theme.default.css";
 const OwlCarousel = dynamic(import("react-owl-carousel"), { ssr: false });
 
 const CarouselSmall = ({ trending }) => {
+  // const [flag, setFlag] = useState(0);
   console.log("trending");
   console.log(trending);
+
   return (
     <div className="slider-active nav-a">
-      {trending && trending.length && trending[0].views}
-      <OwlCarousel
-        className="owl-theme"
-        loop
-        items={1}
-        dots={false}
-        margin={10}
-        autoplay
-        autoplayTimeout={3000}
-        autoplayHoverPause
-      >
-        {trending.map((blog) => {
-          return (
-            <div class="item" key={blog._id}>
-              <div className="single-slider">
-                <div className="trending-top ">
-                  <div className="trend-top-img carouselSmall">
-                    <img src={blog.mainImg} alt="" />
-                    <div className="trend-top-cap" style={{ width: "80%" }}>
-                      <span
-                        className="bgr"
-                        data-animation="fadeInUp"
-                        data-delay=".2s"
-                        data-duration="1000ms"
-                      >
-                        {blog.category}
-                      </span>
-                      <div>
-                        <a
-                          href="latest_news.html"
+      {/* {trending && trending.length && trending[0].views} */}
+      {trending && (
+        <OwlCarousel
+          className="owl-theme"
+          loop
+          items={1}
+          dots={false}
+          margin={10}
+          autoplay
+          autoplayTimeout={3000}
+          autoplayHoverPause
+        >
+          {trending.map((blog) => {
+            return (
+              <div class="item" key={blog._id}>
+                <div className="single-slider">
+                  <div className="trending-top ">
+                    <div className="trend-top-img carouselSmall">
+                      <img src={blog.mainImg} alt="" />
+                      <div className="trend-top-cap" style={{ width: "80%" }}>
+                        <span
+                          className="bgr"
                           data-animation="fadeInUp"
-                          data-delay=".4s"
+                          data-delay=".2s"
                           data-duration="1000ms"
                         >
-                          {blog.title}
-                        </a>
+                          {blog.category}
+                        </span>
+                        <div>
+                          <a
+                            href="latest_news.html"
+                            data-animation="fadeInUp"
+                            data-delay=".4s"
+                            data-duration="1000ms"
+                          >
+                            {blog.title}
+                          </a>
+                        </div>
+                        <p
+                          data-animation="fadeInUp"
+                          data-delay=".6s"
+                          data-duration="1000ms"
+                        >
+                          Views: {blog.views} | {blog.createdDate}
+                        </p>
                       </div>
-                      <p
-                        data-animation="fadeInUp"
-                        data-delay=".6s"
-                        data-duration="1000ms"
-                      >
-                        Views: {blog.views} | {blog.createdDate}
-                      </p>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-          );
-        })}
-      </OwlCarousel>
+            );
+          })}
+        </OwlCarousel>
+      )}
     </div>
   );
 };
