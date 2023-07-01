510c0abab8b44edf999d115990e212685df7dbcd
MOHDSHEES <mohd.shees100@gmail.com>
2023-07-01 17:01:49 +0530

    goggle analytics

--- pages/_app.js
+++ pages/_app.js
@@ -17,6 +17,7 @@ import { Router } from "next/dist/client/router";
 import NProgress from "nprogress";
 import "nprogress/nprogress.css";
 import "react-loading-skeleton/dist/skeleton.css";
+import Script from "next/script";
 import MainFooter from "../components/footer/mainFooter";
 
 NProgress.configure({ showSpinner: false });
@@ -44,6 +45,15 @@ export default function App({ Component, pageProps, trending }) {
   }, []);
   return (
     <>
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+       (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+       new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+       j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+       'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+       })(window,document,'script','dataLayer','GTM-WH4KP2P');
+      `}
+      </Script>
       <MyProvider initialData={trending}>
         <CompleteNavbar />
         <Component {...pageProps} />
--- pages/_document.js
+++ pages/_document.js
@@ -8,10 +8,20 @@ class MyDocument extends Document {
           <link rel="icon" href="/icons/favicon.ico" />
           <link rel="manifest" href="/icons/manifest.json" />
           <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
+          <meta
+            name="google-site-verification"
+            content="KDX93825cyhwPt5qrouMT2SW88-urVI4NOO5zpJjEmc"
+          />
         </Head>
         <body>
           <Main />
           <NextScript />
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WH4KP2P"
+      height="0" width="0" style="display:none;visibility:hidden">`,
+            }}
+          />
         </body>
       </Html>
     );
