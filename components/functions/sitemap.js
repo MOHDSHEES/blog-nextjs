9a1e32eb2cbe7c46c5baf67d4094b34e2e658704
MOHDSHEES <mohd.shees100@gmail.com>
2023-07-03 01:27:12 +0530

    sitemap added

---
+++ components/functions/sitemap.js
@@ -0,0 +1,48 @@
+function generateSiteMap(posts) {
+  const URL = "https://www.offtheweb.in";
+  return `<?xml version="1.0" encoding="UTF-8"?>
+     <urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
+       <url>
+         <loc>${URL}</loc>
+       </url>
+       <url>
+         <loc>${URL}/policies/advertise</loc>
+       </url>
+        <url>
+         <loc>${URL}/contact</loc>
+       </url>
+       <url>
+       <loc>${URL}/career</loc>
+     </url>
+     <url>
+     <loc>${URL}/category/Technology</loc>
+   </url>
+   <url>
+   <loc>${URL}/category/Cyber-Security</loc>
+ </url>
+ <url>
+ <loc>${URL}/category/Business</loc>
+</url>
+<url>
+<loc>${URL}/category/Sports</loc>
+</url>
+<url>
+<loc>${URL}/policies/privacyPolicy</loc>
+</url>
+<url>
+<loc>${URL}/policies/termsCondition</loc>
+</url>
+       ${posts
+         .map((post) => {
+           return `
+             <url>
+                 <loc>${`${URL}/blog/${post.title}/${post._id}`}</loc>
+             </url>
+           `;
+         })
+         .join("")}
+     </urlset>
+   `;
+}
+
+export { generateSiteMap };
---
+++ pages/sitemap.xml.js
@@ -0,0 +1,27 @@
+// import { getSortedPostsData } from "../lib/posts";
+
+import { generateSiteMap } from "../components/functions/sitemap";
+import dbConnect from "../lib/mongoose";
+import blogModel from "../models/blogModel";
+
+export async function getServerSideProps({ res }) {
+  //   const posts = getSortedPostsData();
+  await dbConnect();
+  const blogs = await blogModel.find({}, { _id: 1, title: 1 }).lean();
+  //   let da = blogs.toObject();
+
+  const data = blogs.map((obj) => ({ ...obj, _id: obj._id.toString() }));
+  // Generate the XML sitemap with the blog data
+  const sitemap = generateSiteMap(data);
+
+  res.setHeader("Content-Type", "text/xml");
+  // Send the XML to the browser
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+}
+
+export default function SiteMap() {}
