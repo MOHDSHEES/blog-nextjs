6b4b166385abeb6d1320d791b270d7937d4578be
MOHDSHEES <mohd.shees100@gmail.com>
2023-07-16 19:43:53 +0530

    bug

--- next.config.js
+++ next.config.js
@@ -2,13 +2,10 @@
 const webpack = require("webpack");
 const nextConfig = {
   reactStrictMode: true,
-  outputFileTracing: false,
   images: {
     domains: ["res.cloudinary.com"],
   },
-  serverRuntimeConfig: {
-    serverTimeout: 10000, // Set the timeout to 10 seconds
-  },
+
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.plugins.push(
       new webpack.ProvidePlugin({
