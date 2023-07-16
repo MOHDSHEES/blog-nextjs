1c747e21ae57cb3b75ccaaffa7bbc4d3ccc5136f
MOHDSHEES <mohd.shees100@gmail.com>
2023-07-16 16:33:55 +0530

    bug

--- pages/blogs/[title].js
+++ pages/blogs/[title].js
@@ -137,6 +137,9 @@ const BlogDetail = ({ data, imgUrl }) => {
   //     // setloading(false);
   //   })();
   // }, [data]);
+  useEffect(() => {
+    setUpdatedData(data);
+  }, [data]);
 
   let flag = 1;
   useEffect(() => {
